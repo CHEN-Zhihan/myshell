5c5253621c0a21f0e31b4e2b309371726fdd56c5
nikola <nikola@nikoladeMacBook-Pro.local>
2016-09-26 13:18:43 +0800

    finish the main function

--- co-version/myshell.c
+++ co-version/myshell.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#define BUFFER_SIZE 1026
+
+
 int main(int argc, char const *argv[]) {
-	/* code */
+	char buffer[BUFFER_SIZE];
+	while (true) {
+		fprintf(stdout, "## myshell $ ");
+		int error = get_command(buffer);//get the row command.
+		//TODO::::::::::
+		//1.handle the error and format the buffer.
+		//2. determine whether is internal command
+		//3. parse the command.
+		//4. run the command.
+	}
 	return 0;
 }
